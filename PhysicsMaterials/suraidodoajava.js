47469527172856460bd699583903f8991c3e85c6
yurenzi <b1446520@planet.kanazawa-it.ac.jp>
2017-02-02 21:58:22 +0900

    testupload2

---
+++ PhysicsMaterials/suraidodoajava.js
@@ -0,0 +1,34 @@
+﻿
+private var opened : boolean;
+private var near : boolean;
+private var animator : Animator;
+function Start () {
+    opened = false;
+    near = false;
+    animator = transform.parent.GetComponent(Animator);
+}
+function Update () {
+    if(Input.GetKeyDown("space") && near) {
+        Debug.Log("space");
+        if(!opened) {
+            animator.SetBool("Open", true);
+            Open();
+            Debug.Log("Open");
+        } else {
+            animator.SetBool("Open", false);
+            Open();
+            Debug.Log("Close");
+        }
+    }
+}
+function OnTriggerEnter(col : Collider) {
+    if(col.tag == "Player") {
+        near = true;
+    }
+}
+    function OnTriggerExit(col : Collider) {
+        near = false;
+    }
+        function Open() {
+            opened = !opened;
+        }
